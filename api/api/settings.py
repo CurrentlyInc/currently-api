5992a9a959988a87fc4c95a72e4c7ed3f573a920
N4D1K-lgtm <77400755+N4D1K-lgtm@users.noreply.github.com>
2023-01-23 00:03:10 -0500

    Initialized GraphQL Django APP

--- api/api/settings.py
+++ api/api/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'graphene_django',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {   
+    "SCHEMA": "main.schema.schema"
+}
